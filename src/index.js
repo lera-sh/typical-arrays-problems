1c15880ec7d13985eaae8d09e994d161915fa3f7
Valeria Shirko <valerka.shirko@gmail.com>
2020-10-01 00:36:16 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0
+
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0
+
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0
+
+  return array.reduce((acc, curr) => acc + curr, 0) / array.length
 }
